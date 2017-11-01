f529aee2e983f082329107e85ad753eb7763d7b8
sundarsy <sundarsy@gmail.com>
2017-11-01 19:13:01 +0530

    Security and Vault added

---
+++ reactivespringkotlin/src/main/kotlin/com/bytesville/tutorial/reactivespringkotlin/config/SecurityConfig.kt
@@ -0,0 +1,25 @@
+package com.bytesville.tutorial.reactivespringkotlin.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.core.userdetails.MapUserDetailsRepository
+import org.springframework.security.core.userdetails.User
+import javax.annotation.PostConstruct
+
+@Configuration
+@EnableWebFluxSecurity
+class SecurityConfig {
+
+    @Value("\${X-Service-Pass}")
+    val pass:String?=null;
+
+    @Bean
+    @PostConstruct
+    fun users () = MapUserDetailsRepository(User.
+            withUsername("bytesville")
+           .password(pass)
+            .roles("ADMIN", "USER")
+            .build());
+}
